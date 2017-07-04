d4b1c91adcb46a88d7b44626e671f2344ecce4b2
Pedro Alves <pedromarcelodesaalves@gmail.com>
2017-07-04 06:50:31 -0300

    Updated: renaming providers config keys

--- config/providers.php
+++ config/providers.php
@@ -2,13 +2,13 @@
 
 return [
     [
-        'register' => new Silex\Provider\MonologServiceProvider(),
+        'provider' => new Silex\Provider\MonologServiceProvider(),
         'options' => [
             'monolog.logfile' => 'php://stderr',
         ],
     ],
     [
-        'register' => new Silex\Provider\TwigServiceProvider(),
+        'provider' => new Silex\Provider\TwigServiceProvider(),
         'options' => [
             'twig.path' => dirname(__DIR__) . '/views'
         ],
