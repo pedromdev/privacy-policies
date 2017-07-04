11ac3c502e549e06be8c54e495f640fe7fdad274
Pedro Alves <pedromarcelodesaalves@gmail.com>
2017-07-04 08:17:43 -0300

    Updated: removing var_dump

--- src/Actions/PrivacyPolicies.php
+++ src/Actions/PrivacyPolicies.php
@@ -7,7 +7,6 @@ class PrivacyPolicies
     function __invoke($name)
     {
         $filename = dirname(__DIR__) . "/views/barware/{$name}.html";
-        var_dump($filename);die;
         return file_get_contents($filename);
     }
 }
